7efdda06798c58acc5a818743dc8f2942fd7bee0
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-18 10:49:18 +0900

    update 13
    [add]add calc
    
    addition Column calc and Raw calc

--- 13Spreadsheet.py
+++ 13Spreadsheet.py
@@ -1,2 +1,16 @@
 #   表の行数rと列数c、r × c の要素を持つ表を読み込んで、各行と列の合計を挿入した新しい表を出力するプログラム
 r, c = map(int, input().split())
+spreadSheet = [[0 for i in range(c + 1)] for j in range(r + 1)]
+#   表入力の受付
+for i in range(0, r):
+    spreadSheet[i][:c] = map(int, input().split())
+    #   print(spreadSheet)
+#   行方向合計の算出
+for i in range(0, r):
+    for j in range(0, c):
+        spreadSheet[i][c] += spreadSheet[i][j]
+#   列方向合計の算出
+for i in range(0, c + 1):
+    for j in range(0, r):
+        spreadSheet[r][i] += spreadSheet[j][i]
+print(spreadSheet)
