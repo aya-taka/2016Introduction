59cf64e3edff5e58cadb02ae3ad37e646bd81fbe
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-18 14:53:14 +0900

    update 17
    [modify]change output variable data

--- 17CountingCharacters.py
+++ 17CountingCharacters.py
@@ -2,26 +2,19 @@
 #   標準入力から文字列読み込み
 
 import sys
-
+#   改行を含む文字列を読み込み
 input_char = []
 for line in sys.stdin:
     input_char.append(line)
-temp = "".join(input_char)
-count_char = "".join(temp)
-print(count_char[2])
+#   文字列をすべて結合
+count_char = "".join(input_char)
+#   print(count_char[2])
+#   文字カウント用配列の定義
 countingChar = [0 for i in range(26)]
-for i in range(0, len(input_char)):
-    if ord(input_char[i].lower()) - ord("a") > -1 and ord(input_char[i].lower()) - ord("a") < 27:
-        countingChar[ord(input_char[i].lower()) - ord("a")] += 1
+#   カウントアップ処理
+for i in range(0, len(count_char)):
+    if ord(count_char[i].lower()) - ord("a") > -1 and ord(count_char[i].lower()) - ord("a") < 27:
+        countingChar[ord(count_char[i].lower()) - ord("a")] += 1
+#   出力
 for i in range(0, 26):
     print("{0} : {1}".format(chr(ord("a") + i), countingChar[i]))
-
-'''
-input_char = list(input())
-countingChar = [0 for i in range(26)]
-for i in range(0, len(input_char)):
-    if ord(input_char[i].lower()) - ord("a") > -1 and ord(input_char[i].lower()) - ord("a") < 27:
-        countingChar[ord(input_char[i].lower()) - ord("a")] += 1
-for i in range(0, 26):
-    print("{0} : {1}".format(chr(ord("a") + i), countingChar[i]))
-'''
