7cdf27e84f69f8a32f5d4629738fc88d8bfb8b91
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-18 12:23:02 +0900

    update 16
    [add]calc and output
    
    calc digit total and output digit total

--- 16SumofNumbers.py
+++ 16SumofNumbers.py
@@ -1,1 +1,13 @@
 #   与えられた数の各桁の和を計算するプログラム
+while 1:
+    #   標準入力から数値読み込み
+    input_num = list(input())
+    #   終了判定　"0"入力で終了
+    if int(input_num[0]) == 0:
+        break
+    #   print(input_num)
+    #   各桁合計を保存する変数
+    digitTotal = 0
+    for i in range(0, len(input_num)):
+        digitTotal += int(input_num[i])
+    print("{0}".format(digitTotal))
