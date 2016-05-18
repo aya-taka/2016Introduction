97c6b026d0160e2cf9c32c490e8f905a44108cf5
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-18 17:23:05 +0900

    update 20
    [add]calc and output

--- 20Shuffle.py
+++ 20Shuffle.py
@@ -10,7 +10,15 @@ while 1:
     if temp.isalpha:
         temp = "".join(temp).split()
     cardList += temp
-print(cardList)
+#   print(cardList)
 for i in range(len(cardList)):
+    if cardList[i] == "-":
+        break
     if cardList[i].isalpha():
-        pass
+        editCard = cardList[i]
+        for j in range(i + 2, int(cardList[i + 1]) + i + 2):
+            if cardList[j].isalpha() or cardList[j] == "-":
+                break
+            editCard = editCard + editCard
+            editCard = editCard[int(cardList[j]):len(cardList[i]) + int(cardList[j])]
+        print("{0}".format(editCard))
