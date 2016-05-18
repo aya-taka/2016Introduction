74ace83726b2dbdb3d0bb3b93acd702099b1ffed
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-18 11:20:46 +0900

    update 14
    [add]calc C table and output
    [rename]sheet_zxz to A or B
    
    C table = A x B
    A and B is designation in quiz

--- 14MatrixMultiplication.py
+++ 14MatrixMultiplication.py
@@ -1,14 +1,31 @@
 #   n×mの行列 A と m×l の行列 B を入力し、それらの積である n×l の行列 C を出力するプログラム
 n, m, l = map(int, input().split())
 #   n x m 行列の生成
-sheet_nxm = [[0 for i in range(m)] for j in range(n)]
+A = [[0 for i in range(m)] for j in range(n)]
 #   m x l 行列の生成
-sheet_mxl = [[0 for i in range(l)] for j in range(m)]
+B = [[0 for i in range(l)] for j in range(m)]
+#   n x l 行列(出力行列)の生成
+C = [[0 for i in range(l)] for j in range(n)]
+
 #   m x n 行列を標準入力から読み込む
 for i in range(0, n):
-    sheet_nxm[i][:m] = map(int, input().split())
+    A[i][:m] = map(int, input().split())
     #   print(sheet_nxm)
 #   m x n 行列を標準入力から読み込む
 for i in range(0, m):
-    sheet_mxl[i][:l] = map(int, input().split())
+    B[i][:l] = map(int, input().split())
     #   print(sheet_mxl)
+#   C(n x l)行列の計算　A行列とB行列の積
+for i in range(0, n):
+    for j in range(0, l):
+        for k in range(0, m):
+            C[i][j] += A[i][k] * B[k][j]
+#   print(C)
+#   C行列の出力
+for i in range(0, n):
+    for j in range(0, l):
+        print("{0}".format(C[i][j]), end="")
+        if j != l - 1:
+            print(" ", end="")
+        else:
+            print("")
