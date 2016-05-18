d2927ec95bc6112008363a0cb6f7c0ef90ad2136
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-18 15:37:09 +0900

    update 19
    [add]input
    
    add input W & T

--- 19FindingaWord.py
+++ 19FindingaWord.py
@@ -1,1 +1,12 @@
 #   1つの単語 W と文章 T ,T の中にある W の数を出力するプログラム
+import sys
+#   単語Wの取得
+w = input()
+#   文章Tの定義
+t = []
+#   文章Tの取得
+for line in sys.stdin:
+    t.append(line)
+    if line.find("END_OF_TEXT") == -1:
+        break
+print(t)
