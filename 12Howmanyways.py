8409e1d05cc5f9344c90ffeb5a9b4a8c8daf349a
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-18 10:15:04 +0900

    update 12
    [Modify]Modify in input
    
    input add split

--- 12Howmanyways.py
+++ 12Howmanyways.py
@@ -1,6 +1,6 @@
 #   1~nまでの間の3つの数値を足し合わせた数がx組み合わせの数を求めるプログラム
 while 1:
-    n, x = map(int, input())
+    n, x = map(int, input().split())
     if n == 0 and x == 0:
         break
     ansCount = 0
@@ -9,3 +9,4 @@ while 1:
             for k in range(1, n):
                 if i + j + k == x:
                     ansCount += 1
+    print("{0}".format(ansCount))
