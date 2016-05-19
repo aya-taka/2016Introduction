1b33d9c91217b18ec8f097abfa51acb6603c7935
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-20 08:54:27 +0900

    update 24
    [add]search dice top and front and output

--- 24DiceII.py
+++ 24DiceII.py
@@ -9,6 +9,7 @@ class Dice:
         self.left = 4
         self.back = 5
         self.bottom = 6
+    #   2->1->5->6
     def turnSouth(self):
         temp = self.top
         self.top = self.back
@@ -16,6 +17,7 @@ class Dice:
         self.bottom = self.front
         self.front = temp
         return self
+    #   4->1->3->6
     def turnEast(self):
         temp = self.top
         self.top = self.left
@@ -23,6 +25,7 @@ class Dice:
         self.bottom = self.right
         self.right = temp
         return self
+    #   3->1->4->6
     def turnWest(self):
         temp = self.top
         self.top = self.right
@@ -30,12 +33,30 @@ class Dice:
         self.bottom = self.left
         self.left = temp
         return self
+
+    #   5->1->2->6
     def turnNorth(self):
         temp = self.top
         self.top = self.front
         self.front = self.bottom
         self.bottom = self.back
         self.back = temp
+        return self
+    #   2->3->5->4
+    def turnRight(self):
+        temp = self.front
+        self.front = self.right
+        self.right = self.back
+        self.back = self.left
+        self.left = temp
+        return self
+    #   2->4->5->3
+    def turnRight(self):
+        temp = self.front
+        self.front = self.left
+        self.left = self.back
+        self.back = self.right
+        self.right = temp
         return self
 
 #   Diceの生成
@@ -50,3 +71,19 @@ for i in range(0, q):
     #   質問の読み込み 上面(1) 前面(2)　
     question = list(input())
     question = "".join(question).split()
+    #   上面の割り出し　前後方向
+    for j in range(0, 3):
+        if question[0] == myDice.top:
+            break
+        myDice.turnNorth()
+    #   上面の割り出し　左右方向
+    for j in range(0, 3):
+        if question[0] == myDice.top:
+            break
+        myDice.turnEast()
+    #   正面の割り出し　上面を変えずに回転
+    for j in range(0, 3):
+        if question[1] == myDice.front:
+            break
+        myDice.turnRight()
+    print("{0}".format(myDice.right))
