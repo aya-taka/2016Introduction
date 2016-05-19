3bb0e722432589195323b62bb4ca0d3c17ee251b
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-19 11:37:13 +0900

    update 22
    [add]calc p=1~3 range

--- 22DistanceII.py
+++ 22DistanceII.py
@@ -1,7 +1,7 @@
 #   ２つの n 次元ベクトルから、p がそれぞれ 1、2、3、∞∞ のミンコフスキー距離を求めるプログラム
 import math
 #   次元数nを標準入力から取得
-n = input()
+n = int(input())
 #   ベクトルxを標準入力から取得
 x = list(input())
 x = "".join(x).split()
@@ -9,6 +9,18 @@ x = "".join(x).split()
 y = list(input())
 y = "".join(y).split()
 #   p=1,2,無限大の時のミンコフスキー距離を格納する配列
-dxy = []
+#   合わせて初期化
+dxy = [0 for i in range(4)]
+#   p=1の時のミンコフスキー距離の計算
 for i in range(0, n):
-    pass
+    dxy[0] += math.fabs(int(x[i]) - int(y[i]))
+#   p=2
+for i in range(0, n):
+    dxy[1] += (math.fabs(int(x[i]) - int(y[i]))) ** 2
+dxy[1] = math.sqrt(dxy[1])
+#   p=3
+for i in range(0, n):
+    dxy[2] += (math.fabs(int(x[i]) - int(y[i]))) ** 3
+dxy[2] = math.pow(dxy[2], 1.0 / 3.0)
+
+print(dxy[2])
