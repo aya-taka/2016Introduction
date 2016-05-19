da100a92ed38a492f272a04b7c2a4aac629d8db5
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-19 09:03:34 +0900

    update 21
    [add]input p
    
    p is transformation order number

--- 21Transformation.py
+++ 21Transformation.py
@@ -1,6 +1,9 @@
 #   文字列 str に対して、与えられた命令の列を実行するプログラム
 
+#   加工対象文字列strを標準入力から取得
 str = list(input())
+#   加工命令回数pを標準入力から取得
+p = int(input())
 str = "".join(str).split()
 if str[0] == "print":
     print(str[0])
