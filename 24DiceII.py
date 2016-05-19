731420834343bff03f2f7b69e6e51c58a9fd0b3d
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-19 15:28:43 +0900

    update 24
    [add]no change condition

--- 24DiceII.py
+++ 24DiceII.py
@@ -6,11 +6,16 @@ dice = [0 for i in range(6)]
 dice = list(input().split())
 #   質問の数qを標準入力から取得
 q = int(input())
-
 #   ループによる質問の読み込みと出力
-#   出力対象 右側面(3)
+#   出力対象 右側面 ダイステーブル換算(3)
 for i in range(0, q):
     #   質問の読み込み 上面(1) 前面(2)　
     question = list(input())
     question = "".join(question).split()
+    #   東西方向に移動した形跡があるか
+    if question[0] == dice[0] and question[1] == dice[1]:
+        print("{0}".format(dice[2]))
+    #   南北方向に移動した形跡があるか
+    elif question[1] != dice[1]:
+        pass
     print(question)
