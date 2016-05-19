d2fcd636fd44472003d41941393addf06cb2df9d
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-19 12:25:17 +0900

    update 23
    [add]all if process and output dice's top

--- 23DiceI.py
+++ 23DiceI.py
@@ -10,13 +10,30 @@ order = list(input())
 for i in range(0, len(order)):
     #   S:5->1->2->6,5が1,1が2,,,
     if order[i] == "S":
-        pass
+        temp = dice[0]
+        dice[0] = dice[4]
+        dice[4] = dice[5]
+        dice[5] = dice[1]
+        dice[1] = temp
     #   E:4->1->3->6
     elif order[i] == "E":
-        pass
+        temp = dice[0]
+        dice[0] = dice[3]
+        dice[3] = dice[5]
+        dice[5] = dice[2]
+        dice[2] = temp
     #   W:3->1->4->6
     elif order[i] == "W":
-        pass
+        temp = dice[0]
+        dice[0] = dice[2]
+        dice[2] = dice[5]
+        dice[5] = dice[3]
+        dice[3] = temp
     #   N:2->1->5->6
     elif order[i] == "N":
-        pass
+        temp = dice[0]
+        dice[0] = dice[1]
+        dice[1] = dice[5]
+        dice[5] = dice[4]
+        dice[4] = temp
+print("{0}".format(dice[0]))
