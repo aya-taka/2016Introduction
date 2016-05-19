d24329852bf0cfc6c5e56182d624a4592debead5
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-19 09:29:39 +0900

    update 21
    [modify]print target

--- 21Transformation.py
+++ 21Transformation.py
@@ -2,6 +2,7 @@
 
 #   加工対象文字列strを標準入力から取得
 str = list(input())
+str = "".join(str)
 #   加工命令回数pを標準入力から取得
 p = int(input())
 #   p個の命令を保持する配列の定義
@@ -10,9 +11,9 @@ for i in range(0, p):
     orderList[i] = list(input())
     orderList[i] = "".join(orderList[i]).split()
     if orderList[i][0] == "print":
-        print("{0}".format(str[int(orderList[i][1]) - 1:int(orderList[i][2]) - 1]))
+        print("{0}".format(str[int(orderList[i][1]) - 1:int(orderList[i][2])]))
     elif orderList[i][0] == "reverse":
         pass
     elif orderList[i][0] == "replace":
         pass
-    print(str)
+    print(str[1])
