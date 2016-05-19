794295a84e70315550601b580f0b9bed24bd8177
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-19 14:41:01 +0900

    update 24
    [add]input question

--- 24DiceII.py
+++ 24DiceII.py
@@ -5,8 +5,11 @@ dice = [0 for i in range(6)]
 #   ダイスの面を標準入力から取得
 dice = list(input().split())
 #   質問の数を標準入力から取得
-question_number = input()
+question_number = int(input())
 
 #   ループによる質問の読み込みと出力
 for i in range(0, question_number):
-    pass
+    #   質問の読み込み
+    question = list(input())
+    question = "".join(question).split()
+    print(question)
