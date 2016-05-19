acd1a22cf731ec25b7ecab3cbab5fb3bc8d7318a
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-19 09:22:02 +0900

    update 21
    [add]order comand split

--- 21Transformation.py
+++ 21Transformation.py
@@ -7,9 +7,12 @@ p = int(input())
 #   p個の命令を保持する配列の定義
 orderList = [0 for i in range(p)]
 for i in range(0, p):
+    orderList[i] = list(input())
+    orderList[i] = "".join(orderList[i]).split()
     if str[0] == "print":
         print(str[0])
     elif str[0] == "reverse":
         pass
     elif str[0] == "replace":
         pass
+    print(orderList[i])
