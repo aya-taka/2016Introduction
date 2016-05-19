4f38351e8edcfdc6e7f1ae930c379e8f2e6d2adf
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-19 09:01:57 +0900

    update 21
    [modify]remove while loop

--- 21Transformation.py
+++ 21Transformation.py
@@ -1,11 +1,10 @@
 #   文字列 str に対して、与えられた命令の列を実行するプログラム
 
-while 1:
-    str = list(input())
-    str = "".join(str).split()
-    if str[0] == "print":
-        print(str[0])
-    elif str[0] == "reverse":
-        pass
-    elif str[0] == "replace":
-        pass
+str = list(input())
+str = "".join(str).split()
+if str[0] == "print":
+    print(str[0])
+elif str[0] == "reverse":
+    pass
+elif str[0] == "replace":
+    pass
