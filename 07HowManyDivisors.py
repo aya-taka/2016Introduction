c15af42806ca75049bb28986e4dbcd27d07b2203
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-20 13:23:44 +0900

    update 07
    [fix]change divisor count to cnt

--- 07HowManyDivisors.py
+++ 07HowManyDivisors.py
@@ -1,11 +1,8 @@
 #   ３つの整数 a、b、c を読み込み、a から b までの整数の中に、c の約数がいくつあるかを求めるプログラム
 a, b, c = map(int, input().split())
 #   約数の数をカウントアップする変数
-divisor_count = 0
-#   cを割る変数　初期値a、範囲a<DivideNum<b
-divide_num = a
+cnt = 0
 for i in range(a, b + 1):
-    if c % divide_num == 0:
-        divisor_count += 1
-    divide_num += 1
-print("{0}".format(divisor_count))
+    if c % i == 0:
+        cnt += 1
+print("{0}".format(cnt))
