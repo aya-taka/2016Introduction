c7fcc0fa6a8000592311edf39e41e1a936d33027
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-20 11:07:55 +0900

    update 06
    [fix]remove "()" in while and if
    [add]indention after if condition

--- 06PrintTestCases.py
+++ 06PrintTestCases.py
@@ -1,7 +1,8 @@
 #   １つの整数 x を読み込み、それをそのまま出力するプログラム
 i = 1
-while(1):
+while 1:
     x = int(input())
-    if(x == 0):break
+    if x == 0:
+        break
     print("Case {0}: {1}".format(i, x))
     i += 1
