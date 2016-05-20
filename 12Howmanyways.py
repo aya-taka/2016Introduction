bf3c13db8498c449bd7d74f98dc45ee1cf1317fc
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-20 13:33:08 +0900

    update 12
    [fix]rename ansCount to cnt

--- 12Howmanyways.py
+++ 12Howmanyways.py
@@ -3,11 +3,11 @@ while 1:
     n, x = map(int, input().split())
     if n == 0 and x == 0:
         break
-    ansCount = 0
+    cnt = 0
     for i in range(1, n + 1):
         for j in range(i, n + 1):
             for k in range(j, n + 1):
                 if i + j + k == x and i != j and i != k and j != k:
-                    ansCount += 1
+                    cnt += 1
                     #   print("{0} {1} {2}".format(i, j, k))
-    print("{0}".format(ansCount))
+    print("{0}".format(cnt))
