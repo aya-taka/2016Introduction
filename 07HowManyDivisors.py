b9bafe8e9ec0320d2148e988b9c8808e94e53ae1
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-20 11:19:13 +0900

    update 07
    [fix]variable rename

--- 07HowManyDivisors.py
+++ 07HowManyDivisors.py
@@ -1,11 +1,11 @@
 #   ３つの整数 a、b、c を読み込み、a から b までの整数の中に、c の約数がいくつあるかを求めるプログラム
 a, b, c = map(int, input().split())
 #   約数の数をカウントアップする変数
-DivisorCount = 0
+divisor_count = 0
 #   cを割る変数　初期値a、範囲a<DivideNum<b
-DivideNum = a
+divide_num = a
 for i in range(a, b + 1):
-    if c % DivideNum == 0:
-        DivisorCount += 1
-    DivideNum += 1
-print("{0}".format(DivisorCount))
+    if c % divide_num == 0:
+        divisor_count += 1
+    divide_num += 1
+print("{0}".format(divisor_count))
