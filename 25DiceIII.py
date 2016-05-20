4ece38ea95706fa4d1c883c192dc6c4fe707432d
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-20 10:34:38 +0900

    update 25
    [fix]change Dice class

--- 25DiceIII.py
+++ 25DiceIII.py
@@ -1,91 +1,85 @@
 #   2つのサイコロが同一のものか判定するプログラム
 
 #   ダイスの面を定義するクラス
+#   top = 1 front = 2 right = 3 left = 4 back = 5 bottom = 6
 class Dice:
-    def __init__(self):
-        self.top = 0
-        self.front = 0
-        self.right = 0
-        self.left = 0
-        self.back = 0
-        self.bottom = 0
+    def __init__(self, nums):
+        self.face = nums
     #   2->1->5->6
     def turnSouth(self):
-        temp = self.top
-        self.top = self.back
-        self.back = self.bottom
-        self.bottom = self.front
-        self.front = temp
+        temp = self.face[0]
+        self.face[0] = self.face[4]
+        self.face[4] = self.face[5]
+        self.face[5] = self.face[1]
+        self.face[1] = temp
         return self
     #   4->1->3->6
     def turnEast(self):
-        temp = self.top
-        self.top = self.left
-        self.left = self.bottom
-        self.bottom = self.right
-        self.right = temp
+        temp = self.face[0]
+        self.face[0] = self.face[2]
+        self.face[2] = self.face[5]
+        self.face[5] = self.face[3]
+        self.face[3] = temp
         return self
     #   3->1->4->6
     def turnWest(self):
-        temp = self.top
-        self.top = self.right
-        self.right = self.bottom
-        self.bottom = self.left
-        self.left = temp
+        temp = self.face[0]
+        self.face[0] = self.face[3]
+        self.face[3] = self.face[5]
+        self.face[5] = self.face[2]
+        self.face[2] = temp
         return self
     #   5->1->2->6
     def turnNorth(self):
-        temp = self.top
-        self.top = self.front
-        self.front = self.bottom
-        self.bottom = self.back
-        self.back = temp
-        return self
+        temp = self.face[0]
+        self.face[0] = self.face[1]
+        self.face[1] = self.face[5]
+        self.face[5] = self.face[4]
+        self.face[4] = temp
     #   2->3->5->4
     def turnRight(self):
-        temp = self.front
-        self.front = self.right
-        self.right = self.back
-        self.back = self.left
-        self.left = temp
+        temp = self.face[1]
+        self.face[1] = self.face[2]
+        self.face[2] = self.face[4]
+        self.face[4] = self.face[3]
+        self.face[3] = temp
         return self
     #   2->4->5->3
     def turnLeft(self):
-        temp = self.front
-        self.front = self.left
-        self.left = self.back
-        self.back = self.right
-        self.right = temp
+        temp = self.face[1]
+        self.face[1] = self.face[3]
+        self.face[3] = self.face[4]
+        self.face[4] = self.face[2]
+        self.face[2] = temp
         return self
     def equals(self, Dice):
-        if (self.top == Dice.top and self.front == Dice.front and self.bottom == Dice.bottom and
-                    self.back == Dice.back and self.right == Dice.right and self.left == Dice.left):
+        if self.face == Dice.face:
             return True
         else:
             return False
 
 #   Diceの生成
-Dice1 = Dice()
-Dice2 = Dice()
-#   ダイス初期状態の入力
-Dice1.top, Dice1.front, Dice1.right, Dice1.left, Dice1.back, Dice1.bottom = input().split()
-Dice2.top, Dice2.front, Dice2.right, Dice2.left, Dice2.back, Dice2.bottom = input().split()
+Dice1 = Dice(input().split())
+Dice2 = Dice(input().split())
 
 #   上面の割り出し　前後方向
 for i in range(0, 3):
-    if Dice1.top == Dice2.top:
+    if Dice1.face[0] == Dice2.face[0]:
         break
     Dice2.turnNorth()
+#    print("1", Dice2.face)
 #   上面の割り出し　左右方向
 for i in range(0, 3):
-    if Dice1.top == Dice2.top:
+    if Dice1.face[0] == Dice2.face[0]:
         break
     Dice2.turnEast()
+#    print("2", Dice2.face)
 #   正面の割り出し　上面を変えずに回転
 for i in range(0, 3):
-    if Dice1.front == Dice2.front:
+    if Dice1.face[1] == Dice2.face[1]:
         break
     Dice2.turnRight()
+#    print("3", Dice2.face)
 if Dice1.equals(Dice2):
     print("Yes")
 else:
