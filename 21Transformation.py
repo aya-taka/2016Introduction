d9233e1b8c7789d83f1d7e879e9f90ded135f55d
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-20 16:26:57 +0900

    update 21
    [add]comment

--- 21Transformation.py
+++ 21Transformation.py
@@ -10,9 +10,16 @@ orderList = [0 for i in range(p)]
 for i in range(0, p):
     orderList[i] = list(input())
     orderList[i] = "".join(orderList[i]).split()
+    #   "print"に対する処理
+    #   printの次の数値から、そのさらに次の数値までの文字列を出力
     if orderList[i][0] == "print":
         print("{0}".format(str[int(orderList[i][1]):int(orderList[i][2]) + 1]))
+    #   "reverse"に対する処理
+    #   reverseの次の数値から、そのさらに次の数値までの文字列を反転
+    #   s = s[:a] + s[a:b+1][::-1] + s[b+1:]がきれい
     elif orderList[i][0] == "reverse":
         str = str[0:int(orderList[i][1])] + str[-len(str) + int(orderList[i][2]):-len(str) + int(orderList[i][1]) - 1:-1] + str[int(orderList[i][2]) + 1:]
+    #   "replace"に対する処理
+    #   指定された範囲を、指定された文字列で置き換える
     elif orderList[i][0] == "replace":
         str = str[:int(orderList[i][1])] + orderList[i][3] + str[int(orderList[i][2]) + 1:]
