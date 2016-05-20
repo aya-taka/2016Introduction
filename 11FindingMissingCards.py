68792638c1327fb4d8d9971889d162a3ea36149c
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-20 13:32:10 +0900

    update 11
    [fix]rename aaBbb to aa_bbb

--- 11FindingMissingCards.py
+++ 11FindingMissingCards.py
@@ -1,18 +1,18 @@
-haveTrumps = [[0 for i in range(13)] for j in range(4)]
-allCards = int(input())
-for i in range(0, allCards):
-    cardType, cardNum = input().split()
-    if cardType == "S":
-        haveTrumps[0][int(cardNum) - 1] = 1
-    elif cardType == "H":
-        haveTrumps[1][int(cardNum) - 1] = 1
-    elif cardType == "C":
-        haveTrumps[2][int(cardNum) - 1] = 1
-    elif cardType == "D":
-        haveTrumps[3][int(cardNum) - 1] = 1
+have_trumps = [[0 for i in range(13)] for j in range(4)]
+all_cards = int(input())
+for i in range(0, all_cards):
+    card_type, card_num = input().split()
+    if card_type == "S":
+        have_trumps[0][int(card_num) - 1] = 1
+    elif card_type == "H":
+        have_trumps[1][int(card_num) - 1] = 1
+    elif card_type == "C":
+        have_trumps[2][int(card_num) - 1] = 1
+    elif card_type == "D":
+        have_trumps[3][int(card_num) - 1] = 1
 for i in range(0, 4):
     for j in range(0, 13):
-        if haveTrumps[i][j] == 0:
+        if have_trumps[i][j] == 0:
             if i == 0:
                 print("S ", end="")
             elif i == 1:
