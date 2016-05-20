ad3b9341d7a0c808b0b2f824eff2a5e468d684e7
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-20 14:44:41 +0900

    update 25
    [fix]add indention
    
    indention add to front class and func

--- 25DiceIII.py
+++ 25DiceIII.py
@@ -1,10 +1,12 @@
 #   2つのサイコロが同一のものか判定するプログラム
 
+
 #   ダイスの面を定義するクラス
 #   top = 1 front = 2 right = 3 left = 4 back = 5 bottom = 6
 class Dice:
     def __init__(self, nums):
         self.face = nums
+
     #   2->1->5->6
     def turnSouth(self):
         temp = self.face[0]
@@ -13,6 +15,7 @@ class Dice:
         self.face[5] = self.face[1]
         self.face[1] = temp
         return self
+
     #   4->1->3->6
     def turnEast(self):
         temp = self.face[0]
@@ -21,6 +24,7 @@ class Dice:
         self.face[5] = self.face[3]
         self.face[3] = temp
         return self
+
     #   3->1->4->6
     def turnWest(self):
         temp = self.face[0]
@@ -29,6 +33,7 @@ class Dice:
         self.face[5] = self.face[2]
         self.face[2] = temp
         return self
+
     #   5->1->2->6
     def turnNorth(self):
         temp = self.face[0]
@@ -36,6 +41,7 @@ class Dice:
         self.face[1] = self.face[5]
         self.face[5] = self.face[4]
         self.face[4] = temp
+
     #   2->3->5->4
     def turnRight(self):
         temp = self.face[1]
@@ -44,6 +50,7 @@ class Dice:
         self.face[4] = self.face[3]
         self.face[3] = temp
         return self
+
     #   2->4->5->3
     def turnLeft(self):
         temp = self.face[1]
@@ -52,27 +59,28 @@ class Dice:
         self.face[4] = self.face[2]
         self.face[2] = temp
         return self
-    def equals(self, Dice):
+
+    def equals(self, another):
         #   上面の割り出し　前後方向
         for i in range(0, 4):
-            if self.face[0] == Dice.face[0]:
+            if self.face[0] == another.face[0]:
                 #   正面の割り出し　上面を変えずに回転
                 for j in range(0, 4):
-                    if self.face[1] == Dice.face[1]:
-                        if self.face == Dice.face:
+                    if self.face[1] == another.face[1]:
+                        if self.face == another.face:
                             return True
-                    Dice.turnRight()
-            Dice.turnNorth()
+                    another.turnRight()
+                another.turnNorth()
         #   上面の割り出し　左右方向
         for i in range(0, 4):
-            if self.face[0] == Dice.face[0]:
+            if self.face[0] == another.face[0]:
                 #   正面の割り出し　上面を変えずに回転
                 for j in range(0, 4):
-                    if self.face[1] == Dice.face[1]:
-                        if self.face == Dice.face:
+                    if self.face[1] == another.face[1]:
+                        if self.face == another.face:
                             return True
-                    Dice.turnRight()
-            Dice.turnEast()
+                    another.turnRight()
+            another.turnEast()
         return False
 
 #   Diceの生成
