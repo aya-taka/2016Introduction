9a413d6f2d2474e6000960a8573f560a5cd42a02
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-20 11:11:01 +0900

    update 07
    [add]comment in variable

--- 07HowManyDivisors.py
+++ 07HowManyDivisors.py
@@ -1,6 +1,8 @@
 #   ３つの整数 a、b、c を読み込み、a から b までの整数の中に、c の約数がいくつあるかを求めるプログラム
 a, b, c = map(int, input().split())
+#   約数の数をカウントアップする変数
 DivisorCount = 0
+#   cを割る変数　初期値a、範囲a<DivideNum<b
 DivideNum = a
 for i in range(a, b + 1):
     if c % DivideNum == 0:
