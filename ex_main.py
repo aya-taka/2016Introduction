a25e1165fc545388535f0c0e638bbcdf27d43ca7
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-23 09:47:05 +0900

    update ex_main
    [add]request

--- ex_main.py
+++ ex_main.py
@@ -1,4 +1,20 @@
+# -*- coding: utf-8 -*-
+import sys
 import json
 import requests
 
 import ex_accesskey
+
+word = input().split()
+
+query = {
+    "format": "json",
+    "keyid": ex_accesskey.keyid,
+    "freeword": word,
+    "hit_per_page": 3
+}
+
+r = requests.get(ex_accesskey.url, params=query)
+
+print("r:{0}".format(r))
+print("r.json:{0}".format(json.dumps(r.json(), sort_keys=True, indent=2)))
