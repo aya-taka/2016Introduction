092820dea14521c4c23249b66c5a95718d909a18
hayashizakitakaaki <ground.fleet@gmail.com>
2016-05-23 15:26:21 +0900

    update ex_main
    [add]comment

--- ex_main.py
+++ ex_main.py
@@ -13,6 +13,8 @@ import ex_accesskey
 #   定義
 #   クラス　API
 #   init()  言語を指定し、キーIDとURLを指定
+#   get_json()  json形式のデータの取得
+#   get_name()  json形式のデータからname要素を抽出
 import ex_create_request
 #   検索対象文字列の読み込み
 #   フリーワード検索に使用
@@ -34,8 +36,8 @@ else:
 query = {
     "format": "json",
     "keyid": request_url.key,
-    "freeword": word,
 }
+query["freeword"] = word
 #   リクエストを送信し、json形式を取得
 request_url.get_json(query)
 
